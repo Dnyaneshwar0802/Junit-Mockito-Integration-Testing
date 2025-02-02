0663526d17cebee18fa77e3b26f21c498f050300
GroupProject-D <dnyaneshwarbagal0802@gmail.com>
2025-02-02 06:41:24 +0530

    Added Some comments to understand repo layer

--- src/main/java/com/test/repository/EmployeeRepository.java
+++ src/main/java/com/test/repository/EmployeeRepository.java
@@ -9,9 +9,16 @@ import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-
+    /*
+    Below Method is SB feature findByUsername means it find by username column from table in DB and
+   Matches and return Employee if username is valid otherwise return Empty instead of null
+   */
    Optional <Employee> findByUsername(String username);
-
+   /*
+  Below Method is Custom Query But using JPQL
+  inside JPQL you dont need to use @Param annotation alos it is index base not the variable based
+  ?1 means it accept value from first parameter etc
+ */
   @Query("SELECT e FROM Employee e WHERE e.username = ?1 AND e.name = ?2")
   Optional<Employee> findByUsernameAndName(String username,String name);
 
