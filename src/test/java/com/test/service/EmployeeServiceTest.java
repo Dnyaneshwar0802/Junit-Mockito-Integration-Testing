9b9d7180d6d2ce557582df772df03e899605359f
GroupProject-D <dnyaneshwarbagal0802@gmail.com>
2025-02-08 14:05:35 +0530

    SAVE EMPLOYEE METHOD SEVICE LAYER TEST

--- src/test/java/com/test/service/EmployeeServiceTest.java
+++ src/test/java/com/test/service/EmployeeServiceTest.java
@@ -1,11 +1,17 @@
 package com.test.service;
 
+import com.test.model.Employee;
 import com.test.repository.EmployeeRepository;
 import com.test.serviceImpl.EmployeeServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTest {
@@ -13,5 +19,18 @@ public class EmployeeServiceTest {
     EmployeeRepository employeeRepository;
     @InjectMocks
     EmployeeServiceImpl employeeService;
+    @Test
+    @DisplayName("SAVE EMPLOYEE METHOD SEVICE LAYER TEST")
+    public void givenEmployee_whenSaveEmployee_thenReturnEmployee() {
+        //given
+        Employee employee = Employee.builder().id(1).name("Dnyaneshwar").username("db").salary(4000).build();
+        BDDMockito.given(employeeRepository.save(employee)).willReturn(employee);
+        //when
+        Employee savedEmployee1 = employeeService.saveEmployee(employee);
+        System.out.println(savedEmployee1.toString());
+        //then
+        Assertions.assertThat(savedEmployee1).isNotNull();
+        Assertions.assertThat(savedEmployee1.getId()).isEqualTo(1);
+    }
 
 }
